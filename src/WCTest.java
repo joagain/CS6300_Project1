5b208793f63205380689d43e01c1ab41a8723fe0
Gaurav <narang.gaurav18@gmail.com>
2014-01-21 19:44:58 -0500

    Added the framework for testing

---
+++ src/WCTest.java
@@ -0,0 +1,39 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by palindrome on 1/21/14.
+ */
+public class WCTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testCount() throws Exception {
+
+    }
+
+    @Test
+    public void testFormat_count() throws Exception {
+
+    }
+
+    @Test
+    public void testMain() throws Exception {
+
+    }
+
+    @Test
+    public void testParseCommandLine () throws Exception {
+
+    }
+}
