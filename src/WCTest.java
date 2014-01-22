cfb9e804df7980a0c27b836faf47284a77a2898c
hoyin29 <hoyin29@users.noreply.github.com>
2014-01-22 14:28:00 -0500

    Update WCTest.java
    fixed conflicts

--- src/WCTest.java
+++ src/WCTest.java
@@ -7,50 +7,125 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.Date;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import Errors.InputError;
 
 public class WCTest {
+	
+    @Before
+    public void setUp() throws Exception {
 
-    @Test
-    public void testCountNonzero() throws Exception {
-        WC counter = new WC(new String[]{"java", "WC", "src"+File.separator+"test01.txt", "-D", "*"});
-        int result = (int) counter.count();
-        assertTrue("test01.txt expected average of 9, but got " + result, result == 9);
     }
 
-    @Test(expected = InputError.class)
-    public void testCountZero() throws Exception {
-        WC counter = new WC(new String[]{"java", "WC", "src"+File.separator+"test02.txt", "-D", "*"});
-        int result = (int) counter.count();
-        assertTrue("test02.txt expected average of 0, but got " + result, result == 0);
+    @After
+    public void tearDown() throws Exception {
+
     }
 
-    @Test(expected = InputError.class)
-    public void testParseCommandLineError() throws InputError {
-        WC counter = new WC(new String[]{"java", "WC", "src"+File.separator+"test01.txt", "-A", "@"});
-        counter = new WC(new String[]{"java", "WC", "src"+File.separator+"test01.txt", "-B", "&"});
-        counter = new WC(new String[]{"java", "WC", "src"+File.separator+"test01.txt", "-C", "%"});
-    }
-
+    // test 1
     @Test
     public void testParseCommandLineNoError() throws InputError {
         WC counter = new WC(new String[]{"java", "WC", "src"+File.separator+"test01.txt", "-D", "#"});
         counter = new WC(new String[]{"java", "WC", "src"+File.separator+"test01.txt", "-D", "$"});
         counter = new WC(new String[]{"java", "WC", "src"+File.separator+"test01.txt", "-D", "^"});
     }
+    
+    // test 2
+    @Test(expected=InputError.class)
+    public void testBadArgument() throws InputError {
+    	WC counter = new WC(new String[]{"java", "WC", "src\\test01.txt", "-D", "@", "-L", "M"});
+    }
+    
+    // test 3
+    @Test(expected=InputError.class)
+    public void testParseCommandLineError() throws InputError {
+    	WC counter = new WC(new String[]{"java", "WC", "src\\test01.txt", "-A", "@"});
+    }
+    
+    // test 4
+    @Test(expected=NullPointerException.class)
+    public void testNoInputFile() {
+    	try {
+    		WC counter = new WC(new String[]{"java", "WC", null});
+    	}
+    	catch(InputError ie) {
+    		//System.out.println(ie.getMessage());
+    		assertTrue("incorrect error encountered", ie.getMessage().compareTo("[Input error] ERR04 - No input file provided. Please refer to the user manual for further information.") == 0);
+    	}
+    }
+    
+    // test 5
+    @Test
+    public void testInputDirectory() {
+    	try {
+    		WC counter = new WC(new String[]{"java", "WC", "src"});
+    	}
+    	catch(InputError ie) {
+    		//System.out.println(ie.getMessage());
+    		assertTrue("incorrect error encountered", ie.getMessage().compareTo("[Input error] ERR05 - The file is a directory. Please refer to the user manual for further information.") == 0);
+    	}
+    }
+    
+    // test 6
+    @Test
+    public void testParseEmptyFile() {
+    	try {
+    		WC counter = new WC(new String[]{"java", "WC", "src"+File.separator+"test02.txt"});
+    	}
+    	catch(InputError ie) {
+    		//System.out.println(ie.getMessage());
+    		assertTrue("incorrect error encountered", ie.getMessage().compareTo("[Input error] ERR06 - The file provided was empty. Please refer to the user manual for further information.") == 0);
+    	}
+    }
+    
+    // test 7
+    @Test
+    public void testNoSuchFileExist() {
+    	try {
+    		WC counter = new WC(new String[]{"java", "WC", "i"});
+    	}
+    	catch(InputError ie) {
+    		//System.out.println(ie.getMessage());
+    		assertTrue("incorrect error encountered", ie.getMessage().compareTo("[Input error] ERR07 - No such file exists. Please refer to the user manual for further information.") == 0);
+    	}
+    }
+    
+    // test 8
+    @Test
+    public void testReadingContent() throws InputError {
+    	WC counter = new WC(new String[]{"java", "WC", "src\\test01.txt", "-D", "#"});
+    }
+    
+    // test 9
+    @Test
+    public void testCountNonzero() throws Exception {
+    	WC counter = new WC(new String[]{"java", "WC", "src\\test01.txt", "-D", "*"});
+    	int result = (int)counter.count();
+    	assertTrue("test01.txt expected average of 9, but got " + result, result == 9);
+    }
+    
+    // test 10
+    @Test(expected=InputError.class)
+    public void testCountZero() throws Exception {
+    	WC counter = new WC(new String[]{"java", "WC", "src"+File.separator+"test02.txt", "-D", "*"});
+    	int result = (int)counter.count();
+    	assertTrue("test02.txt expected average of 0, but got " + result, result == 0);
 
+    	counter = new WC(new String[]{"java", "WC", "src"+File.separator+"test01.txt", "-D", "*"});
+        result = (int) counter.count();
+        assertTrue("test01.txt expected average of 9, but got " + result, result == 9);
+    }
+
+    // test 11
     @Test
     public void testNonEmptyFile() throws InputError {
         WC counter = new WC(new String[]{"java", "WC", "src"+File.separator+"test01.txt", "-D", "#"});
     }
 
-    @Test(expected = InputError.class)
-    public void testParseEmptyFile() throws InputError {
-        WC counter = new WC(new String[]{"java", "WC", "src"+File.separator+"test02.txt", "-D", "#"});
-    }
-
+    // test 12
     @Test
     public void testMultipleDelimiters() throws InputError {
         WC counter = new WC(new String[]{"java", "WC", "src"+File.separator+"test03.txt", "-D", "#$"});
@@ -65,6 +140,7 @@ public class WCTest {
         counter = new WC(new String[]{"java", "WC", "src"+File.separator+"test01.txt", "-D", "#$%^"});
     }
 
+    // test 13
     @Test
     public void testCounterSpeed() throws Exception {
         WC counter = new WC(new String[]{"java", "WC", "src"+File.separator+"test01.txt", "-D", "#$"});
@@ -76,6 +152,7 @@ public class WCTest {
         assertTrue("Expected to finish the test in less than "+counter.getWords()/5000 +" seconds", time_taken < (double)counter.getWords()/5000);
     }
 
+    // test 14
     @Test
     public void testFileCorruption() throws Exception {
 
