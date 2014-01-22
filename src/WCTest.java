0222e4f42e171f6d8ff9392bf68ac1f8015f50a9
Gaurav <narang.gaurav18@gmail.com>
2014-01-22 13:43:40 -0500

    Removed testCounterSpeed and some reformatting

--- src/WCTest.java
+++ src/WCTest.java
@@ -1,4 +1,3 @@
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -6,10 +5,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.Date;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import Errors.InputError;
@@ -18,79 +14,70 @@ public class WCTest {
 
     @Test
     public void testCountNonzero() throws Exception {
-    	WC counter = new WC(new String[]{"java", "WC", "src\\test01.txt", "-D", "*"});
-    	int result = (int)counter.count();
-    	assertTrue("test01.txt expected average of 9, but got " + result, result == 9);
-    }
-    
-    @Test(expected=InputError.class)
-    public void testCountZero() throws Exception {
-    	WC counter = new WC(new String[]{"java", "WC", "src\\test02.txt", "-D", "*"});
-    	int result = (int)counter.count();
-    	assertTrue("test02.txt expected average of 0, but got " + result, result == 0);
+        WC counter = new WC(new String[]{"java", "WC", "src\\test01.txt", "-D", "*"});
+        int result = (int) counter.count();
+        assertTrue("test01.txt expected average of 9, but got " + result, result == 9);
     }
 
-    @Test(expected=InputError.class)
-    public void testParseCommandLineError() throws InputError {
-    	WC counter = new WC(new String[]{"java", "WC", "src\\test01.txt", "-A", "@"});
-    	counter = new WC(new String[]{"java", "WC", "src\\test01.txt", "-B", "&"});
-    	counter = new WC(new String[]{"java", "WC", "src\\test01.txt", "-C", "%"});
+    @Test(expected = InputError.class)
+    public void testCountZero() throws Exception {
+        WC counter = new WC(new String[]{"java", "WC", "src\\test02.txt", "-D", "*"});
+        int result = (int) counter.count();
+        assertTrue("test02.txt expected average of 0, but got " + result, result == 0);
     }
-    
+
+    @Test(expected = InputError.class)
+    public void testParseCommandLineError() throws InputError {
+        WC counter = new WC(new String[]{"java", "WC", "src\\test01.txt", "-A", "@"});
+        counter = new WC(new String[]{"java", "WC", "src\\test01.txt", "-B", "&"});
+        counter = new WC(new String[]{"java", "WC", "src\\test01.txt", "-C", "%"});
+    }
+
     @Test
     public void testParseCommandLineNoError() throws InputError {
-    	WC counter = new WC(new String[]{"java", "WC", "src\\test01.txt", "-D", "#"});
-    	counter = new WC(new String[]{"java", "WC", "src\\test01.txt", "-D", "$"});
-    	counter = new WC(new String[]{"java", "WC", "src\\test01.txt", "-D", "^"});
+        WC counter = new WC(new String[]{"java", "WC", "src\\test01.txt", "-D", "#"});
+        counter = new WC(new String[]{"java", "WC", "src\\test01.txt", "-D", "$"});
+        counter = new WC(new String[]{"java", "WC", "src\\test01.txt", "-D", "^"});
     }
-    
+
     @Test
     public void testNonEmptyFile() throws InputError {
-    	WC counter = new WC(new String[]{"java", "WC", "src\\test01.txt", "-D", "#"});
+        WC counter = new WC(new String[]{"java", "WC", "src\\test01.txt", "-D", "#"});
     }
-    
-    @Test(expected=InputError.class)
+
+    @Test(expected = InputError.class)
     public void testParseEmptyFile() throws InputError {
-    	WC counter = new WC(new String[]{"java", "WC", "src\\test02.txt", "-D", "#"});
+        WC counter = new WC(new String[]{"java", "WC", "src\\test02.txt", "-D", "#"});
     }
-    
+
     @Test
     public void testMultipleDelimiters() throws InputError {
-    	WC counter = new WC(new String[]{"java", "WC", "src\\test03.txt", "-D", "#$"});
-    	int result = (int)counter.count();
-    	assertTrue("test03.txt expected average of 1, but got " + result, result == 1);
-    	
-    	counter = new WC(new String[]{"java", "WC", "src\\test03.txt", "-D", "$"});
-    	result = (int)counter.count();
-    	assertTrue("test03.txt expected average of 3, but got " + result, result == 3);
-    	
-    	counter = new WC(new String[]{"java", "WC", "src\\test01.txt", "-D", "#$%"});
-    	counter = new WC(new String[]{"java", "WC", "src\\test01.txt", "-D", "#$%^"});
+        WC counter = new WC(new String[]{"java", "WC", "src\\test03.txt", "-D", "#$"});
+        int result = (int) counter.count();
+        assertTrue("test03.txt expected average of 1, but got " + result, result == 1);
+
+        counter = new WC(new String[]{"java", "WC", "src\\test03.txt", "-D", "$"});
+        result = (int) counter.count();
+        assertTrue("test03.txt expected average of 3, but got " + result, result == 3);
+
+        counter = new WC(new String[]{"java", "WC", "src\\test01.txt", "-D", "#$%"});
+        counter = new WC(new String[]{"java", "WC", "src\\test01.txt", "-D", "#$%^"});
     }
 
-    @Test
-    public void testCounterSpeed() throws Exception {
-        WC counter = new WC(new String[]{"java", "WC", "src\\test01.txt"});
-        Date start = new Date(), end;
-        counter.count();
-        end = new Date();
-        long time_taken = (end.getTime() - start.getTime())/1000;
-        assertTrue("Expected to finish the test in less than "+counter.getWords()/5000 +" seconds", time_taken < counter.getWords()/5000);
-    }
 
-    
     @Test
     public void testPerformance() throws InputError {
-    	WC counter = new WC(new String[]{"java", "WC", "src\\test04.txt", "-D", "#$"});
-    	Long startTime = new Long(System.currentTimeMillis());
-    	counter.count();
-    	Long endTime = new Long(System.currentTimeMillis());
-    	Long elapsedTime = new Long(endTime.longValue() - startTime.longValue());
-    	assertTrue("test04.txt expected less than a second to process 500 characters, but took " + elapsedTime.longValue(), elapsedTime.compareTo(new Long(1000)) < 0);
+        WC counter = new WC(new String[]{"java", "WC", "src\\test04.txt", "-D", "#$"});
+        Long startTime = new Long(System.currentTimeMillis());
+        counter.count();
+        Long endTime = new Long(System.currentTimeMillis());
+        Long elapsedTime = new Long(endTime.longValue() - startTime.longValue());
+        assertTrue("test04.txt expected less than a second to process 500 characters, but took " + elapsedTime.longValue(), elapsedTime.compareTo(new Long(1000)) < 0);
     }
 
     @Test
-    public void testFileCorruption () throws Exception {
+    public void testFileCorruption() throws Exception {
+        
         WC counter = new WC(new String[]{"java", "WC", "src\\test01.txt"});
         long len = new File("src\\test01.txt").length();
         boolean corrupted = false;
@@ -108,9 +95,10 @@ public class WCTest {
             }
 
             br.close();
-        };
+        }
+        ;
 
-        assertFalse("The file was corrupted during processing",corrupted);
+        assertFalse("The file was corrupted during processing", corrupted);
 
     }
 }
